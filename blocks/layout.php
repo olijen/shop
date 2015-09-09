8e3354a1d0ffc060e6b0f2b1666010882a2f81ac
Eugen Zhylin <e.zhylin@gmail.com>
2015-09-09 18:59:42 +0300

    add style for single page

--- blocks/layout.php
+++ blocks/layout.php
@@ -12,6 +12,7 @@
     <link href="css/main.css" rel="stylesheet">
     <link href="css/header.css" rel="stylesheet">
     <link href="css/footer.css" rel="stylesheet">
+    <link href="../css/single.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
--- blocks/single.php
+++ blocks/single.php
@@ -9,7 +9,21 @@ $id = $routes['3'];
 <h2>Details of <?php echo $cat.$id; ?>:</h2>
 	<div class="product single">
 		<div class="image"><img src="images/<?php echo $cat; ?>.png"></div>
+        <div class="clear"></div>
+        <div class="information">
+            <h2>Названия продукта</h2>
+            <p><span class="features"> First features:</span> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            <hr/>
+            <p><span class="features"> First features:</span> discriptions
+            <hr/>
+            <p><span class="features"> First features:</span> discriptions
+            <hr/>
+            <p><span class="features"> First features:</span> discriptions
+            <hr/>
+            <p><span class="features"> First features:</span> discriptions
+            <hr/>
+        </div>
 		<div class="price">Price: <?php echo $store[$cat][$id]['price']; ?>$</div>
-		<a href="#">Buy</a>
+		<a href="#" class="buy">Buy</a>
 	</div>
 <h2>Reviews:</h2>
