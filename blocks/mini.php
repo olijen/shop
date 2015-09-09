8c14c073f568ffb401819c8c6cff9982172bbb9d
Demid Ganenko <ganenko94@gmail.com>
2015-09-09 18:09:16 +0300

    mini.php commit fix

--- blocks/mini.php
+++ blocks/mini.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 <div class="cards">
-=======
-
->>>>>>> 82788f530802e8535e0d5d0bcd29de4f86ba496f
 <?php
 // Mini-card render
 
@@ -14,11 +10,7 @@ $cat = $routes['2'];
 //d($store['phones']);
 foreach ($store[$cat] as $r) { ?>
 	<div class="product mini">
-<<<<<<< HEAD
-		<div class="image"><img src="images/<?php echo $cat; ?>.png"></div>
-=======
 		<a href="catalog/<?php echo $cat; ?>/<?php echo $r['id']; ?>"><div class="image"><img src="images/<?php echo $cat; ?>.jpg"></div></a>
->>>>>>> 82788f530802e8535e0d5d0bcd29de4f86ba496f
         <div class="raiting">
             <input type="radio">
             <input type="radio">
