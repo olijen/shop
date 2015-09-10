747228cff4c063c53914b1de56b84880a96f5b98
eisenheim94 <ganenko94@gmail.com>
2015-09-10 17:28:44 +0300

    News at main have links and news are can be opened

--- blocks/news.php
+++ blocks/news.php
@@ -7,13 +7,26 @@ require_once('protected/db.php');
 <h2>News</h2>
 <?php
 //d($store['phones']);
-foreach ($news as $r) { ?>
-	<div class="news_item">
-        <h3><?php echo $r['title']; ?></h3>
-        <div class="date"><?php echo $r['date']; ?></div>
-        <p><?php echo $r['full']; ?></p>
-	</div>
-<?php
+if($routes[1] != "news") {
+	foreach ($news as $r) { ?>
+		<div class="news_item">
+			<a href="news/<?php echo $r['id']; ?>">
+			<h3><?php echo $r['title']; ?></h3>
+			<div class="date"><?php echo $r['date']; ?></div>
+			<p><?php echo $r['full']; ?></p>
+			</a>
+		</div>
+	<?php
+	}
+}
+elseif($routes[1] == "news" && isset($routes[2])) {
+	$id = $routes[2]; ?>
+		<div class="news_item">
+			<h3><?php echo $news[$id]['title']; ?></h3>
+			<div class="date"><?php echo $news[$id]['date']; ?></div>
+			<p><?php echo $news[$id]['full']; ?></p>
+		</div>
+	<?php
 }
 ?>
 </div>
