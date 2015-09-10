ddd91241381d3382e491a9b6d9a8a721936aa513
Eugen Zhylin <e.zhylin@gmail.com>
2015-09-10 14:05:34 +0300

    add styli for rating

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 
 $(function(){
-	
-	$('.rating input[type="radio"]').click(function(){
-		var radio = $(this).parent().find('input[type="radio"]');
-		var current = radio.index(this);
-		$(this).parent().find('input[type="radio"]').prop("checked", false).removeAttr("class");
-		for(var i = 0; i < radio.length; i++) {
-			if(i != current) {
-				radio.eq(i).prop("checked", true).addClass("active");;
-			}
-			else {
-				radio.eq(current).prop("checked", true).addClass("active");;
-				break;
-			}
-		}
-	});
-	
+	$('.rating input[type="radio"]').bind({
+        click: (function () {
+            var radio = $(this).parent().find('input[type="radio"]');
+            var current = radio.index(this);
+            $(this).parent().find('input[type="radio"]').prop("checked", false).removeAttr("class");
+            for (var i = 0; i < radio.length; i++) {
+                if (i != current) {
+                    radio.eq(i).prop("checked", true).addClass("active");
+                }
+                else {
+                    radio.eq(current).prop("checked", true).addClass("active");
+                    break;
+                }
+            }
+        })
+    });
 });
