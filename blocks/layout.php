94ae479a0ba0bb5d736ba270f934e91f7332a6d4
Eugen Zhylin <e.zhylin@gmail.com>
2015-09-11 13:53:55 +0300

    errors correction at all files

--- blocks/layout.php
+++ blocks/layout.php
@@ -85,11 +85,9 @@ if(isset($routes[2])) {
                 </div>
             </div>
         </header>
-
         <div class="content">
             <?php renderBlock($block, array('routes' => $routes)); ?>
         </div>
- 
         <footer id="footer">
         	<div class="row">
 				<div class="col-sm-3">
